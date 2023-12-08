76ac40e3775d98edf43e2d47a1e3c6108c32a4f2
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-12-08 23:56:23 +0100

    8th December

---
+++ src/leetCode/ReverseInteger7.java
@@ -0,0 +1,14 @@
+package leetCode;
+
+public class ReverseInteger7 {
+    public int reverse(int x) {
+        //
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        ReverseInteger7 r = new ReverseInteger7();
+        System.out.println(r.reverse(123));
+    }
+}
---
+++ src/leetCode/StringToInteger8.java
@@ -0,0 +1,13 @@
+package leetCode;
+
+public class StringToInteger8 {
+    public int myAtoi(String s) {
+        //
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        StringToInteger8 s = new StringToInteger8();
+        System.out.println(s.myAtoi("42"));
+    }
+}
