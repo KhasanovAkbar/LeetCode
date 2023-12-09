aa8f10c97b9c268a5137ca92d82cccdcf6e7b619
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-12-09 23:52:02 +0100

    9th December

--- src/leetCode/ReverseInteger7.java
+++ src/leetCode/ReverseInteger7.java
@@ -3,12 +3,28 @@ package leetCode;
 public class ReverseInteger7 {
     public int reverse(int x) {
         //
+        int INT32_MIN = Integer.MIN_VALUE;
+        int INT32_MAX = Integer.MAX_VALUE;
 
-        return 0;
+        int sign = (x >= 0) ? 1 : -1;
+        x = Math.abs(x);
+
+        StringBuilder reversedStr = new StringBuilder(Integer.toString(x)).reverse();
+
+        try {
+            int reversedInt = sign * Integer.parseInt(reversedStr.toString());
+
+            if (reversedInt < INT32_MIN || reversedInt > INT32_MAX) {
+                return 0;
+            }
+            return reversedInt;
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 
     public static void main(String[] args) {
         ReverseInteger7 r = new ReverseInteger7();
-        System.out.println(r.reverse(123));
+        System.out.println(r.reverse(-1230));
     }
 }
