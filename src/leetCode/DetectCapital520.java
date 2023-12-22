573640800de17b78595bae59937d956035648ada
KhasanovAkbar <akbar.hasanov0630@gmail.com>
2023-12-22 22:00:21 +0100

    22nd December
    Signed-off-by: KhasanovAkbar <akbar.hasanov0630@gmail.om>

--- src/leetCode/DetectCapital520.java
+++ src/leetCode/DetectCapital520.java
@@ -10,10 +10,10 @@ public class DetectCapital520 {
 
     public static void main(String[] args) {
         DetectCapital520 d = new DetectCapital520();
+        System.out.println(d.detectCapitalUse("fA"));//false
+        System.out.println(d.detectCapitalUse("FlaG"));//false
         System.out.println(d.detectCapitalUse("USA"));//true
         System.out.println(d.detectCapitalUse("leetcode"));//true
         System.out.println(d.detectCapitalUse("Google"));//true
-        System.out.println(d.detectCapitalUse("fA"));//false
-        System.out.println(d.detectCapitalUse("FlaG"));//false
     }
 }
