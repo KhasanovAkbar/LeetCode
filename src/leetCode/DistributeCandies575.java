5568fbed119ffd13bdabe93bf70709f445f79e20
KhasanovAkbar <akbar.hasanov0630@gmail.com>
2023-12-27 22:47:37 +0100

    27th December
    Signed-off-by: KhasanovAkbar <akbar.hasanov0630@gmail.om>

---
+++ src/leetCode/DistributeCandies575.java
@@ -0,0 +1,17 @@
+package leetCode;
+
+public class DistributeCandies575 {
+    public int distributeCandies(int[] candyType) {
+        //
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        DistributeCandies575 d = new DistributeCandies575();
+        int[] nums = {1, 1, 2, 2, 3, 3}; //3
+//        int[] nums = {1, 1, 2, 3}; //2
+//        int[] nums = {6, 6, 6, 6}; //1
+        System.out.println(d.distributeCandies(nums));
+    }
+}
