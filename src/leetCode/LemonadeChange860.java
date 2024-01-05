62d542ab4b8a5469b438fc8510ed168938fd4d70
KhasanovAkbar <akbar.hasanov0630@gmail.com>
2024-01-05 20:40:09 +0100

    5th January
    Signed-off-by: KhasanovAkbar <akbar.hasanov0630@gmail.om>

---
+++ src/leetCode/LemonadeChange860.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+public class LemonadeChange860 {
+    public boolean lemonadeChange(int[] bills) {
+        //
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LemonadeChange860 l = new LemonadeChange860();
+        int[] temp = {5, 5, 5, 10, 20};
+        System.out.println(l.lemonadeChange(temp));//true
+    }
+}
