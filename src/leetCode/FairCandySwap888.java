875ff28781a385840cd7b96f9cd6e55197cb2591
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2024-01-06 23:58:23 +0100

    6th January
    Signed-off-by: KhasanovAkbar <akbar.hasanov0630@gmail.om>

---
+++ src/leetCode/FairCandySwap888.java
@@ -0,0 +1,20 @@
+package leetCode;
+
+public class FairCandySwap888 {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        //
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        FairCandySwap888 f = new FairCandySwap888();
+        int[] num1 = {1, 1};
+        int[] num2 = {2, 2};
+        int[] ints = f.fairCandySwap(num1, num2);
+        for (int anInt : ints) {
+            System.out.print(anInt + " ");
+        }
+    }
+}
