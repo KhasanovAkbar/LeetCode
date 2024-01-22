21e8c86123882b750420c7071d53ebf04cdee58f
KhasanovAkbar <akbar.hasanov0630@gmail.com>
2024-01-23 00:59:20 +0100

    23rd January
    Signed-off-by: KhasanovAkbar <akbar.hasanov0630@gmail.om>

---
+++ src/leetCode/TransposeMatrix867.java
@@ -0,0 +1,20 @@
+package leetCode;
+
+public class TransposeMatrix867 {
+    public int[][] transpose(int[][] matrix) {
+        //
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[][] nums = {{}, {}};
+        TransposeMatrix867 t = new TransposeMatrix867();
+        int[][] transpose = t.transpose(nums);
+        for (int i = 0; i < transpose.length; i++) {
+            for (int j = 0; j < transpose[i].length; j++) {
+                System.out.println();
+            }
+        }
+    }
+}
